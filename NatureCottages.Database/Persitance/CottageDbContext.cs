e5f795d4a7b746aecde265ba22f7efe83facf81f
billy mumby <billy.mumby@outlook.com>
2018-10-18 10:15:18 +0100

    added more to the structure

---
+++ NatureCottages.Database/Persitance/CottageDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using NatureCottages.Database.Domain;
+
+namespace NatureCottages.Database.Persitance
+{
+    public class CottageDbContext : DbContext
+    {
+
+        public DbSet<Booking> Bookings { get; set; }
+
+        public DbSet<Attraction> Attractions { get; set; }
+
+        public DbSet<Cottage> Cottages { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
