ff930530db2fa4407dec6f25254a8abbf0c448fc
billy mumby <billy.mumby@outlook.com>
2019-03-28 20:54:22 +0000

    fixed the image expanding issue.

---
+++ NatureCottages.ViewModels/Contact/EmailFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace NatureCottages.ViewModels.Contact
+{
+    public class EmailFormViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
